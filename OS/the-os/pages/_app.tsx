4414ae722497221b7f0df4764cfbb3d058d5f251
unknown <pradyumnathakare1@gmail.com>
2023-08-29 03:28:07 +0530

    styled components setup

--- OS/the-os/pages/_app.tsx
+++ OS/the-os/pages/_app.tsx
@@ -1,8 +1,39 @@
-import 'styles/globals.scss'
+
 import type { AppProps } from 'next/app'
-import React from "react";
+import React, { ReactElement } from "react";
+import Head from "next/head";
+import {name} from "package.json";
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
+const GlobalStyle=createGlobalStyle`
+  html,
+ body{
+  padding: 0;
+  margin: 0;
+ }
+ *{
+  box-sizing: border-box;
+ }
+`;
 
-export default function App({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />
+const theme={
+  colors:{
+    primary: '#0070f3'
+  },
+};
+
+export default function App({ Component, pageProps }:AppProps):ReactElement {
+  return( 
+  <>
+  <Head>
+    <title>{name}</title>
+    <meta name="description" content={"The OS project"}/>
+  </Head>
+  <GlobalStyle/>
+  <ThemeProvider theme={theme}>
+  <Component {...pageProps} />
+  </ThemeProvider>
+  </>
+  )
 }
+ 
