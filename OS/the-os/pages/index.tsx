b228f158ed9cb0793ccbe3ab6dc55694d09f63c2
unknown <pradyumnathakare1@gmail.com>
2023-08-29 14:36:19 +0530

    Refractoring Index

--- OS/the-os/pages/index.tsx
+++ OS/the-os/pages/index.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-
-const Title = styled.h1 `
-  font-size: 1.5em;
-  text-align: center;
-  color: ${({theme})=> theme.colors.primary};
-`;
+import Title from "styles/generic/Title";
 
 export default function Home(){
   return<Title>Hello World</Title>
---
+++ OS/the-os/styles/generic/Title.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Title = styled.h1 `
+  font-size: 50px;
+  text-align: center;
+  color: ${({theme})=> theme.colors.primary};
+`;
+export default Title;
